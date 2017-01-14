942837d7aae361d409c34bad48edca889cea843a
Barbosa S. A <samu101108@gmail.com>
2017-01-13 23:48:27 -0300

    Largest Of four Solution

--- largestOfFour.js
+++ largestOfFour.js
@@ -1,10 +1,14 @@
-
 function largestOfFour(arr) {
-  // You can do this!
-  console.log(arr);
+  var results = [];//this empty array will be filled with great numbers
+  for (var i = 0; i < arr.length; i++) {
+    var largestNumber = 0;
+    for (var j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] > largestNumber) {
+        largestNumber = arr[i][j];
+      }
+    }
+    results[i] = largestNumber;
+  }
 
-
-  return arr;
+  return results;
 }
-
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);

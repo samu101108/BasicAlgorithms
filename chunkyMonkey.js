9fb566816bd3c4def050c31cbc78288db7a3ac75
Barbosa S. A <samu101108@gmail.com>
2017-01-30 15:40:17 -0300

    Chunky Monkey solution found!

--- chunkyMonkey.js
+++ chunkyMonkey.js
@@ -1,18 +1,19 @@
 
 function chunkArrayInGroups(arr, size) {
-  // Break it up.
-  var count = 0;
-  var newArr = [];
-  for (var i = 0; i < arr.length; i++) {
-count = count + size;
-    console.log(count);
-arr.slice(count);
-    console.log(arr);
-newArr.push(arr);
-    console.log(newArr);
-
+  var times = arr.length;
+  var sliced = [];
+  var newOne = [];
+  var begin = 0;
+  var end = size;
+  for(var i = 0; i < times; i++){
+    sliced = arr.slice(begin,end); //Method .slice() can take two arguments,
+    begin += size;//that are changed with the second argument of the function call
+    end += size;  //at each loop iteration it adds the 'size' to the 'begin' and 'end'.
+    if(sliced != false){//And if it is not false (I mean empty), it is pushed
+    newOne.push(sliced);//to 'newOne' array.
+    }
+  }
+  return newOne;
 }
-  return newArr;
-}
 
-chunkArrayInGroups(["a", "b", "c", "d"], 2);
+chunkArrayInGroups(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"], 12);
